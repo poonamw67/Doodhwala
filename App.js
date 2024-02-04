30aa40be05eb05d3bac88d6a4b7b076ba1b4fd01
poonam <poonamw67@gmail.com>
2024-02-04 23:22:48 +0530

    2nd save

--- App.js
+++ App.js
@@ -1,10 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const MainComponent = () =>
+
+/*
+AppLayout
+* Header
+  - Title
+  - Login
+* Body
+  - SVG/Video, HamburgerMenu
+  - Cards
+  - CenterImage
+* Footer
+  - Links
+*/
+
+const HeaderComponent = () =>
 {
-    return <h1>test</h1>
+    return (
+      <>
+      <div className='center'>
+      <TitleComponent/>
+      <LoginComponent/>
+      </div>
+      </>
+    )
+}
+
+const TitleComponent = () =>
+{
+  return (
+    <div className="topHeader">
+        <a href='/'>Doodhwalla</a>
+      </div>
+  )
+}
+const LoginComponent = () =>
+{
+  return (
+  <div>
+    <button className="">Login</button>
+  </div>
+)
+}
+
+const AppLayout = () =>
+{
+    return <HeaderComponent/>
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<MainComponent/>);
+root.render(<AppLayout/>);
