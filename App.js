5260087c3b584d19f90b50db05080654709d6769
poonam <poonamw67@gmail.com>
2024-02-04 18:04:01 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const MainComponent = () =>
+{
+    return <h1>test</h1>
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<MainComponent/>);
