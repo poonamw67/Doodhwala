383b1b9f99c643ff7b509d74ea0d983382187675
poonam <poonamw67@gmail.com>
2024-02-08 00:20:07 +0530

    json hompage url corrected

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Body from './components/Body';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+
 const AppLayout = () =>
 {
     return (
@@ -15,4 +16,4 @@ const AppLayout = () =>
     )
 };
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AppLayout/>);
+root.render(<AppLayout />);
